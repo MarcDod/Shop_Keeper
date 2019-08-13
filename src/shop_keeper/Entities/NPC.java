c799584dbde9c9bd9a444fb8f517b931757b3eaa
Caeasarus <julian.reith@arcor.de>
2019-08-13 20:57:33 +0200

    MoveTo & testDraw for NPC

--- src/shop_keeper/Entities/NPC.java
+++ src/shop_keeper/Entities/NPC.java
@@ -5,21 +5,29 @@
  */
 package shop_keeper.Entities;
 
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
+
 /**
  *
  * @author Julian
  */
 public abstract class NPC{
+
     private final String name;
     private int mood;
     protected final int mentality;
     protected Presence presence;
     private final int texture;
-    
-    public enum Presence {
-        HATED,ABSENCE,CUSTOMER,REGULAR;
+    private Point2D.Double coordinates;
+    private double speed;
+
+    public enum Presence{
+        HATED, ABSENCE, CUSTOMER, REGULAR;
     }
-    
+
     public NPC(String name, int texture, int mentality){
         this.name = name;
         this.mood = 0;
@@ -27,6 +35,8 @@ public abstract class NPC{
         this.presence = Presence.CUSTOMER;
         this.texture = texture;
         this.mentality = mentality;
+        this.speed = 1;
+        this.coordinates = new Point2D.Double();
     }
 
     public String getName(){
@@ -43,5 +53,26 @@ public abstract class NPC{
 
     public void setMood(int mood){
         this.mood = mood;
+    }
+
+    public boolean moveTo(Point2D.Double target){
+        double distance = Math.sqrt(coordinates.distance(target));
+        if(this.speed >= distance){
+            this.coordinates.setLocation(target);
+            return true;
+        }else{
+            double scalefactor = distance / speed;
+            double dx = (target.getX() - this.coordinates.getX()) * scalefactor;
+            double dy = (target.getY() - this.coordinates.getY()) * scalefactor;
+            this.coordinates.setLocation(this.coordinates.getX() + dx,
+                    this.coordinates.getY() + dy);
+            return false;
+        }
+    }
+
+    public void testDraw(Graphics2D g2d){
+        Ellipse2D.Double s = new Ellipse2D.Double(this.coordinates.getX() - 5,
+                this.coordinates.getY() - 5, 10, 10);
+        g2d.draw(s);
     }
 }
