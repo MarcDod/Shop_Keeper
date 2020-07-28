a40a0ca0ac50e04ed111112071e76957886b9843
Marc <marc@doderer.com>
2020-07-28 11:19:36 +0200

    Start to create a Menu

---
+++ core/src/de/marcdoderer/shop_keeper/screen/state/MenuState.java
@@ -0,0 +1,49 @@
+package de.marcdoderer.shop_keeper.screen.state;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class MenuState extends State{
+
+
+    @Override
+    public void render(SpriteBatch batch) {
+
+    }
+
+    @Override
+    public void renderShapes(ShapeRenderer shapeRenderer) {
+
+    }
+
+    @Override
+    public void update(float delta) {
+
+    }
+
+    @Override
+    public void resize(float width, float height) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void keyPressed(int keyCode) {
+
+    }
+
+    @Override
+    public void mouseClicked(float x, float y) {
+
+    }
+
+    @Override
+    public OrthographicCamera getCamera() {
+        return null;
+    }
+}
