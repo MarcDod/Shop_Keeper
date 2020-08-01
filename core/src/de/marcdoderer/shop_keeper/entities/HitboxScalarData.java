0ef7d4f8766403f680935f5b7fbda8c6c20fca7e
Caeasarus <julian.reith@arcor.de>
2020-08-01 21:18:18 +0200

    Adding hitbox scalars and updating android tools gradle version
    Signed-off-by: Caeasarus <julian.reith@arcor.de>

---
+++ core/src/de/marcdoderer/shop_keeper/entities/HitboxScalarData.java
@@ -0,0 +1,17 @@
+package de.marcdoderer.shop_keeper.entities;
+
+public class HitboxScalarData {
+    private float scalarX;
+    private float scalarY;
+
+    public HitboxScalarData() {
+    }
+
+    public float getScalarX() {
+        return scalarX;
+    }
+
+    public float getScalarY() {
+        return scalarY;
+    }
+}
